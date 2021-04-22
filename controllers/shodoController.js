099434ab0ac60d0bd9b7bb1b687be4f8f37324f5
Jason Zuang <zuangjason@gmail.com>
2021-04-22 18:29:24 -0500

    working todo list

--- controllers/shodoController.js
+++ controllers/shodoController.js
@@ -1,19 +1,42 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://shodo:shodo@shodo.hxmku.mongodb.net/shodo?retryWrites=true&w=majority')
+//creating schema
+var shodoSchema = new mongoose.Schema({
+    item:String
+});
+
+var Todo = mongoose.model('Shodo',shodoSchema);
+var itemOne = Todo({item:'Fuck the Police'}).save(function(error){
+    if(error) throw error;
+    console.log("item saved");
+});
+
 var data = [{item: 'fuck'},{item:'suck'},{item:'shit out of luck'}];
 var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({extended:false});
 module.exports = function(app){
 
     app.get('/shodo',function(req,res){
-        res.render('todo',{todos: data});
+        //load mdb app and pass 
+        Todo.find({},function(err,data){
+            if(err){ throw err;}
+            res.render('todo',{todos: data});
+        });
+        
     });
     app.post('/shodo', urlencodedParser, function(req,res){
-        data.push(req.body);
-        res.json(data);
+        //get data and load to Todo model
+        var newTodo = Todo(req.body).save(function(err){
+            if(err) throw err;
+            res.json(data);
+        });
+
     });
     app.delete('/shodo/:item', function(req,res){
-          data = data.filter(function(todo){
-            return todo.item.replace(/ /g, '-') !== req.params.item;
-          });
-          res.json(data);
+        Todo.find({item: req.params.item.replace(/\-/g," ")}).deleteOne(function(err,data){
+            if(err) throw err;
+            res.json(data)
+        });
+          
     });
 };
